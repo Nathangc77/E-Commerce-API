fa8dff8d25cf594e21dcf62a7ea3164de1599465
Nathan Moreira <nathangc7@gmail.com>
2024-11-22 11:49:40 -0300

    Put product

--- src/main/java/com/moreira/dscommerce/services/ProductService.java
+++ src/main/java/com/moreira/dscommerce/services/ProductService.java
@@ -34,9 +34,25 @@ public class ProductService {
 
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
-        Product entity = new Product(dto.getId(), dto.getName(), dto.getDescription(), dto.getPrice(), dto.getImgUrl());
+        Product entity = new Product();
+        copyDtoToEntity(dto, entity);
         entity = productRepository.save(entity);
         return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        Product entity = productRepository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
     }
 
 }
